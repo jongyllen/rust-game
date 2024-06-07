b204c974073aa6208f552d3abb6d109ddda89fc1
Jon Gylensward <jon.gyllensward@funnel.io>
2024-06-07 16:03:27 +0200

    first

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use macroquad::prelude::*;
+
+#[macroquad::main("Mitt spel")]
+async fn main() {
+    const MOVEMENT_SPEED: f32 = 200.0;
+
+    let mut x = screen_width() / 2.0;
+    let mut y = screen_height() / 2.0;
+
+    loop {
+        clear_background(DARKPURPLE);
+
+        let delta_time = get_frame_time();
+        if is_key_down(KeyCode::Right) {
+            x += MOVEMENT_SPEED * delta_time;
+        }
+        if is_key_down(KeyCode::Left) {
+            x -= MOVEMENT_SPEED * delta_time;
+        }
+        if is_key_down(KeyCode::Down) {
+            y += MOVEMENT_SPEED * delta_time;
+        }
+        if is_key_down(KeyCode::Up) {
+            y -= MOVEMENT_SPEED * delta_time;
+        }
+
+        x = clamp(x, 0.0, screen_width());
+        y = clamp(y, 0.0, screen_height());
+
+        draw_circle(x, y, 16.0, YELLOW);
+        next_frame().await
+    }
+}
