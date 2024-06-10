a58de2185477c80fc6939b966b3b4a0157a40229
Jon Gyllenswärd <jon@Jons-MacBook-Pro.local>
2024-06-10 22:47:50 +0200

    shader

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,43 @@
 use macroquad::prelude::*;
 
+const FRAGMENT_SHADER: &str = include_str!("starfield-shader.glsl");
+
+const VERTEX_SHADER: &str = "#version 100
+attribute vec3 position;
+attribute vec2 texcoord;
+attribute vec4 color0;
+varying float iTime;
+
+uniform mat4 Model;
+uniform mat4 Projection;
+uniform vec4 _Time;
+
+void main() {
+    gl_Position = Projection * Model * vec4(position, 1);
+    iTime = _Time.x;
+}
+";
+
 struct Shape {
     size: f32,
     speed: f32,
     x: f32,
     y: f32,
+}
+
+impl Shape {
+    fn collides_with(&self, other: &Self) -> bool {
+        self.rect().overlaps(&other.rect())
+    }
+
+    fn rect(&self) -> Rect {
+        Rect {
+            x: self.x - self.size / 2.0,
+            y: self.y - self.size / 2.0,
+            w: self.size,
+            h: self.size,
+        }
+    }
 }
 
 #[macroquad::main("Mitt spel")]
@@ -18,51 +51,96 @@ async fn main() {
         x: screen_width() / 2.0,
         y: screen_height() / 2.0,
     };
-    
+    let mut gameover = false;
     const MOVEMENT_SPEED: f32 = 200.0;
 
+    let mut direction_modifier: f32 = 0.0;
+    let render_target = render_target(320, 150);
+    render_target.texture.set_filter(FilterMode::Nearest);
+    let material = load_material(
+        ShaderSource::Glsl {
+            vertex: VERTEX_SHADER,
+            fragment: FRAGMENT_SHADER,
+        },
+        MaterialParams {
+            uniforms: vec![
+                ("iResolution".to_owned(), UniformType::Float2),
+                ("direction_modifier".to_owned(), UniformType::Float1),
+            ],
+            ..Default::default()
+        },
+    )
+    .unwrap();
+
     loop {
-        clear_background(DARKPURPLE);
+        clear_background(BLACK);
 
-        let delta_time = get_frame_time();
-        if is_key_down(KeyCode::Right) {
-            circle.x += MOVEMENT_SPEED * delta_time;
-        }
-        if is_key_down(KeyCode::Left) {
-            circle.x -= MOVEMENT_SPEED * delta_time;
-        }
-        if is_key_down(KeyCode::Down) {
-            circle.y += MOVEMENT_SPEED * delta_time;
-        }
-        if is_key_down(KeyCode::Up) {
-            circle.y -= MOVEMENT_SPEED * delta_time;
+        material.set_uniform("iResolution", (screen_width(), screen_height()));
+        material.set_uniform("direction_modifier", direction_modifier);
+        gl_use_material(&material);
+        draw_texture_ex(
+            &render_target.texture,
+            0.,
+            0.,
+            WHITE,
+            DrawTextureParams {
+                dest_size: Some(vec2(screen_width(), screen_height())),
+                ..Default::default()
+            },
+        );
+        gl_use_default_material();
+
+        if !gameover {
+
+            let delta_time = get_frame_time();
+            if is_key_down(KeyCode::Right) {
+                circle.x += MOVEMENT_SPEED * delta_time;
+                direction_modifier += 0.05 * delta_time;
+            }
+            if is_key_down(KeyCode::Left) {
+                circle.x -= MOVEMENT_SPEED * delta_time;
+                direction_modifier -= 0.05 * delta_time;
+            }
+            if is_key_down(KeyCode::Down) {
+                circle.y += MOVEMENT_SPEED * delta_time;
+            }
+            if is_key_down(KeyCode::Up) {
+                circle.y -= MOVEMENT_SPEED * delta_time;
+            }
+
+            circle.x = clamp(circle.x, 0.0, screen_width());
+            circle.y = clamp(circle.y, 0.0, screen_height());
+
+            if rand::gen_range(0, 99) >= 95 {
+                let size = rand::gen_range(16.0, 64.0);
+                squares.push(Shape {
+                    size,
+                    speed: rand::gen_range(50.0, 150.0),
+                    x: rand::gen_range(size / 2.0, screen_width() - size / 2.0),
+                    y: -size,
+                });
+            }
+
+            // Move squares
+            for square in &mut squares {
+                square.y += square.speed * delta_time;
+            }
+
+            // Remove squares below bottom of screen
+            squares.retain(|square| square.y < screen_height() + square.size);
         }
 
-        circle.x = clamp(circle.x, 0.0, screen_width());
-        circle.y = clamp(circle.y, 0.0, screen_height());
-
-        draw_circle(circle.x, circle.y, 16.0, YELLOW);
-       
-       
-        if rand::gen_range(0, 99) >= 95 {
-            let size = rand::gen_range(16.0, 64.0);
-            squares.push(Shape {
-                size,
-                speed: rand::gen_range(50.0, 150.0),
-                x: rand::gen_range(size / 2.0, screen_width() - size / 2.0),
-                y: -size,
-            });
+        if squares.iter().any(|square| circle.collides_with(square)) {
+            gameover = true;
         }
 
-        // Move squares
-        for square in &mut squares {
-            square.y += square.speed * delta_time;
+        if gameover && is_key_pressed(KeyCode::Space) {
+            squares.clear();
+            circle.x = screen_width() / 2.0;
+            circle.y = screen_height() / 2.0;
+            gameover = false;
         }
 
-        // Remove squares below bottom of screen
-        squares.retain(|square| square.y < screen_height() + square.size);
-
-        // Draw everything
         draw_circle(circle.x, circle.y, circle.size / 2.0, YELLOW);
         for square in &squares {
             draw_rectangle(
@@ -71,6 +149,18 @@ async fn main() {
                 square.size,
                 square.size,
                 GREEN,
+            );
+        }
+
+        if gameover {
+            let text = "Game Over!";
+            let text_dimensions = measure_text(text, None, 50, 1.0);
+            draw_text(
+                text,
+                screen_width() / 2.0 - text_dimensions.width / 2.0,
+                screen_height() / 2.0,
+                50.0,
+                RED,
             );
         }
 
