583ed317ac315f34f3d7a901ef3262f3ee3fb7b9
Jon Gylensward <jon.gyllensward@funnel.io>
2024-06-11 16:21:52 +0200

    score

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use macroquad::prelude::*;
+use std::fs;
 
 const FRAGMENT_SHADER: &str = include_str!("starfield-shader.glsl");
 
@@ -23,6 +24,7 @@ struct Shape {
     speed: f32,
     x: f32,
     y: f32,
+    collided: bool,
 }
 
 impl Shape {
@@ -45,13 +47,19 @@ async fn main() {
     rand::srand(miniquad::date::now() as u64);
 
     let mut squares = vec![];
+    let mut bullets: Vec<Shape> = vec![];
     let mut circle = Shape {
         size: 32.0,
         speed: MOVEMENT_SPEED,
         x: screen_width() / 2.0,
         y: screen_height() / 2.0,
+        collided: false,
     };
     let mut gameover = false;
+    let mut score: u32 = 0;
+    let mut high_score: u32 = fs::read_to_string("highscore.dat")
+        .map_or(Ok(0), |i| i.parse::<u32>())
+        .unwrap_or(0);
     const MOVEMENT_SPEED: f32 = 200.0;
 
     let mut direction_modifier: f32 = 0.0;
@@ -107,6 +115,15 @@ async fn main() {
             if is_key_down(KeyCode::Up) {
                 circle.y -= MOVEMENT_SPEED * delta_time;
             }
+            if is_key_pressed(KeyCode::Space) {
+                bullets.push(Shape {
+                    x: circle.x,
+                    y: circle.y,
+                    speed: circle.speed * 2.0,
+                    size: 5.0,
+                    collided: false,
+                });
+            }
 
             circle.x = clamp(circle.x, 0.0, screen_width());
             circle.y = clamp(circle.y, 0.0, screen_height());
@@ -118,6 +135,7 @@ async fn main() {
                     speed: rand::gen_range(50.0, 150.0),
                     x: rand::gen_range(size / 2.0, screen_width() - size / 2.0),
                     y: -size,
+                    collided: false,
                 });
             }
 
@@ -125,22 +143,51 @@ async fn main() {
             for square in &mut squares {
                 square.y += square.speed * delta_time;
             }
+            for bullet in &mut bullets {
+                bullet.y -= bullet.speed * delta_time;
+            }
 
-            // Remove squares below bottom of screen
+            // Remove below bottom of screen
             squares.retain(|square| square.y < screen_height() + square.size);
+            bullets.retain(|bullet| bullet.y > 0.0 - bullet.size / 2.0);
+
+            // Remove collided shapes
+            squares.retain(|square| !square.collided);
+            bullets.retain(|bullet| !bullet.collided);
         }
 
+        // Check for collisions
         if squares.iter().any(|square| circle.collides_with(square)) {
+            if score == high_score {
+                fs::write("highscore.dat", high_score.to_string()).ok();
+            }
             gameover = true;
         }
-
+    
+        for square in squares.iter_mut() {
+            for bullet in bullets.iter_mut() {
+                if bullet.collides_with(square) {
+                    bullet.collided = true;
+                    square.collided = true;
+                    score += square.size.round() as u32;
+                    high_score = high_score.max(score);
+                }
+            }
+        }
+    
         if gameover && is_key_pressed(KeyCode::Space) {
             squares.clear();
+            bullets.clear();
             circle.x = screen_width() / 2.0;
             circle.y = screen_height() / 2.0;
             gameover = false;
+            score = 0;
         }
 
+        // Draw everything
+        for bullet in &bullets {
+            draw_circle(bullet.x, bullet.y, bullet.size / 2.0, RED);
+        }
         draw_circle(circle.x, circle.y, circle.size / 2.0, YELLOW);
         for square in &squares {
             draw_rectangle(
@@ -151,10 +198,25 @@ async fn main() {
                 GREEN,
             );
         }
-
+        draw_text(
+            format!("Poäng: {}", score).as_str(),
+            10.0,
+            35.0,
+            25.0,
+            WHITE,
+        );
+        let highscore_text = format!("High score: {}", high_score);
+        let text_dimensions = measure_text(highscore_text.as_str(), None, 25, 1.0);
+        draw_text(
+            highscore_text.as_str(),
+            screen_width() - text_dimensions.width - 10.0,
+            35.0,
+            25.0,
+            WHITE,
+        );
         if gameover {
             let text = "Game Over!";
-            let text_dimensions = measure_text(text, None, 50, 1.0);
+            let text_dimensions = measure_text(text, None, 60, 1.0);
             draw_text(
                 text,
                 screen_width() / 2.0 - text_dimensions.width / 2.0,
